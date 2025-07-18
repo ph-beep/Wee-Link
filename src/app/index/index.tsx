468bfb522857a21317f24c012dd0ed0d7cd1988f
Pedro <pedrobernardoni56@gmail.com>
2025-07-18 17:00:10 -0300

    code init the project

---
+++ src/app/index/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View, StyleSheet } from "react-native"
+import { styles } from "./styles"
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello, World!!</Text>
+        </View>
+    )
+}
+
